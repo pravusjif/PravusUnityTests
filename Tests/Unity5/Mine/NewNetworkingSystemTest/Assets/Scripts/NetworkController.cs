aea891397ec9f5411e621a0f527ac260dbbe4415
pravusjif <pravusjif@gmail.com>
2015-06-22 05:41:26 -0300

    added new networking system test project
    Former-commit-id: 9f7540243892317fe02d3ca7cd6c68e8a06625a3

---
+++ Tests/Unity5/Mine/NewNetworkingSystemTest/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
