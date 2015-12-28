79adde311cde57b020fbc5d780680d77892ce023
pravusjif <pravusjif@gmail.com>
2015-12-27 21:11:46 -0300

    Added my AutomatedTesting tests

---
+++ Tests/Unity5/Mine/AutomatedTesting/Assets/Scripts/Controllers/CharacterJumpController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterJumpController : MonoBehaviour
+{
+    public float jumpForce = 100f;
+
+    private Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space))
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+	}
+}
---
+++ Tests/Unity5/Mine/AutomatedTesting/Assets/Scripts/Editor/Tests/CharacterJumpControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using NSubstitute;
+using UnityEngine;
+using NUnit.Framework;
+
+[TestFixture]
+public class CharacterJumpControllerTest
+{
+    private GameObject characterObject = Substitute.For<GameObject>();
+    //GameObject characterObject = new GameObject();
+}
