03f1271b8fa3844ff8e9264fa8743e4e035d1d3e
pravusjif <pravusjif@gmail.com>
2015-12-29 02:53:50 -0300

    Added CoinGolf proto tests

---
+++ Tests/Unity5/Mine/CoinGolf/CoinGolf/Assets/Scripts/CoinController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CoinController : MonoBehaviour
+{
+    public Vector3 shootingDirection = Vector3.zero;
+    public float shootingForce = 0f;
+    public float maxShootingForce = 20f;
+
+    Vector2 currentTouchPosition;
+    Vector2 lastTouchPosition;
+    Vector2 initialTouchPosition;
+    bool startedAiming = false;
+
+    Rigidbody objectRigidBody = null;
+    List<Touch> touchesList = new List<Touch>();
+
+    void Start()
+    {
+        objectRigidBody = GetComponent<Rigidbody>();
+    }
+
+	void Update ()
+	{
+	    touchesList = InputHelper.GetTouches();
+
+        if (touchesList.Count > 0)
+	    {
+            #if UNITY_EDITOR
+            Ray touchRay = Camera.main.ScreenPointToRay(touchesList[0].position);
+            Debug.DrawRay(touchRay.origin, touchRay.direction * 30, Color.magenta);
+            #endif
+
+            currentTouchPosition = touchesList[0].position;
+
+	        if (startedAiming)
+	        {
+	            if (touchesList[0].phase == TouchPhase.Ended || touchesList[0].phase == TouchPhase.Canceled)
+	                ShootCoin();
+	            else
+	            {
+	                CalculateShootingForce();
+                    CalculateShootingDirection();
+	            }
+	        }
+
+	        lastTouchPosition = currentTouchPosition;
+	    }
+	}
+
+    void CalculateShootingForce()
+    {
+        if (Vector2.Distance(lastTouchPosition, initialTouchPosition) < Vector2.Distance(currentTouchPosition, initialTouchPosition))
+            shootingForce += touchesList[0].deltaPosition.magnitude;
+        else
+            shootingForce -= touchesList[0].deltaPosition.magnitude;
+
+        if (shootingForce > maxShootingForce)
+            shootingForce = maxShootingForce;
+        else if (shootingForce < 0)
+            shootingForce = 0;
+    }
+
+    void CalculateShootingDirection()
+    {
+        shootingDirection.x = (currentTouchPosition - initialTouchPosition).x;
+        shootingDirection.z = (currentTouchPosition - initialTouchPosition).y;
+    }
+
+    void OnMouseDown()
+    {
+        startedAiming = true;
+        initialTouchPosition = InputHelper.GetTouches()[0].position;
+    }
+
+    void ShootCoin()
+    {
+        startedAiming = false;
+
+        shootingDirection.y = 0;
+        objectRigidBody.AddForce(shootingDirection.normalized * shootingForce, ForceMode.Impulse);
+
+        shootingForce = 0;
+    }
+}
---
+++ Tests/Unity5/Mine/CoinGolf/CoinGolf/Assets/Scripts/InputHelper.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class InputHelper : MonoBehaviour
+{
+    static TouchCreator lastFakeTouch;
+
+    public static List<Touch> GetTouches()
+    {
+        List<Touch> touches = new List<Touch>();
+        touches.AddRange(Input.touches);
+
+#if UNITY_EDITOR
+         if(lastFakeTouch == null) lastFakeTouch = new TouchCreator();
+
+         if(Input.GetMouseButtonDown(0))
+         {
+             lastFakeTouch.phase = TouchPhase.Began;
+             lastFakeTouch.deltaPosition = new Vector2(0,0);
+             lastFakeTouch.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+             lastFakeTouch.fingerId = 0;
+         }
+         else if (Input.GetMouseButtonUp(0))
+         {
+             lastFakeTouch.phase = TouchPhase.Ended;
+             Vector2 newPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+             lastFakeTouch.deltaPosition = newPosition - lastFakeTouch.position;
+             lastFakeTouch.position = newPosition;
+             lastFakeTouch.fingerId = 0;
+         }
+         else if (Input.GetMouseButton(0))
+         {
+             lastFakeTouch.phase = TouchPhase.Moved;
+             Vector2 newPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+             lastFakeTouch.deltaPosition = newPosition - lastFakeTouch.position;
+
+             //Debug.Log("DeltaPos: " + lastFakeTouch.deltaPosition.magnitude);
+             /*if (lastFakeTouch.deltaPosition.magnitude <= 0.05f)
+             {
+                lastFakeTouch.phase = TouchPhase.Stationary;
+                Debug.Log("STATIONARY PHASE!");
+             }*/
+
+             lastFakeTouch.position = newPosition;
+             lastFakeTouch.fingerId = 0;
+         }
+         else
+         {
+             lastFakeTouch = null;
+         }
+
+         if (lastFakeTouch != null) touches.Add(lastFakeTouch.Create());
+#endif
+        
+        return touches;
+    }
+}
