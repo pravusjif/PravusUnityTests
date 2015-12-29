a99eea5c21c9df79f4f55f1e293ab499d3e4c28b
pravusjif <pravusjif@gmail.com>
2015-12-29 03:52:11 -0300

    Added Coinhole and basic scene ui

---
+++ Tests/Unity5/Mine/CoinGolf/CoinGolf/Assets/Scripts/CoinHoleController.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+
+public class CoinHoleController : MonoBehaviour
+{
+    SceneUIController UIController;
+
+    void Start()
+    {
+        UIController = GameObject.FindObjectOfType<SceneUIController>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        UIController.DisplayWinningImage();
+    }
+}
---
+++ Tests/Unity5/Mine/CoinGolf/CoinGolf/Assets/Scripts/SceneUIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneUIController : MonoBehaviour
+{
+    public Image winningImage;
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void DisplayWinningImage()
+    {
+        winningImage.enabled = true;
+    }
+}
